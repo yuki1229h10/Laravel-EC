13f275912fe64b9a3c387ec30461636ced2b9859
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-24 17:12:33 +0900

    名前付きスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">header1</x-slot>
     component test1
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">header2</x-slot>
     component test2
 </x-tests.app>
