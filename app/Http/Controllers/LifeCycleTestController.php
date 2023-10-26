3dc0895f8a21c27105bdaafa1c7728d8dc50c622
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-26 19:57:13 +0900

    サービスコンテナ挙動確認

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -14,6 +14,37 @@ class LifeCycleTestController extends Controller
 
         $test = app()->make('lifeCycleTest');
 
+        // サービスコンテナなしのパターン
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+
+        // サービスコンテナ app()ありのパターン
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+
         dd($test, app());
+    }
+}
+
+class Sample
+{
+    public $message;
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+    public function run()
+    {
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send()
+    {
+        echo ('show message');
     }
 }
