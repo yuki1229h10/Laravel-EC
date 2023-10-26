da82b8b545effd43e3697ca19dc08b595afa5aa8
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-26 16:55:56 +0900

    ライフサイクル確認

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    public function showServiceContainerTest()
+    {
+        app()->bind('lifeCycleTest', function () {
+            return 'ライフサイクルテスト';
+        });
+
+        $test = app()->make('lifeCycleTest');
+
+        dd($test, app());
+    }
+}
