bab6fdf2e3fd9447420b38fa68605d62706cb078
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-29 15:21:14 +0900

    view作成

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -23,13 +23,13 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
-        echo $date_now;
-        echo $date_parse;
+        // $date_now = Carbon::now();
+        // $date_parse = Carbon::parse(now());
+        // echo $date_now;
+        // echo $date_parse;
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
 
         // $q_first = DB::table('owners')->select('name')->first();
 
@@ -40,7 +40,8 @@ class OwnersController extends Controller
         // var_dump($q_first);
 
         // dd($e_all, $q_get);
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
