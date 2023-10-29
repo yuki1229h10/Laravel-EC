7d9647985c2ebe6ccbd95dff12541e5114380b0b
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-29 13:17:00 +0900

    Carbon確認 error解決

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner; // Eloquent
 use Illuminate\Support\Facades\DB; // QueryBuilder
+use Carbon\Carbon;
 
 class OwnersController extends Controller
 {
@@ -22,6 +23,11 @@ class OwnersController extends Controller
 
     public function index()
     {
+        $date_now = Carbon::now();
+        $date_parse = Carbon::parse(now());
+        echo $date_now;
+        echo $date_parse;
+
         $e_all = Owner::all();
         $q_get = DB::table('owners')->select('name')->get();
         $q_first = DB::table('owners')->select('name')->first();
--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class AdminSeeder extends Seeder
 {
@@ -17,7 +16,7 @@ class AdminSeeder extends Seeder
     public function run()
     {
         DB::table('admins')->insert([
-            'name' => Str::random(10),
+            'name' => 'test',
             'email' => 'test@test.com',
             'password' => Hash::make('password123'),
             'created_at' => '2021/01/01 11:11:11'
