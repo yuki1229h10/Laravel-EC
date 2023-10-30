ebfed1f8c0372248d78cd82b8b694cf0ba510be7
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-30 19:13:42 +0900

    edit作成

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -41,7 +41,7 @@ class OwnersController extends Controller
         // var_dump($q_first);
 
         // dd($e_all, $q_get);
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
         return view('admin.owners.index', compact('owners'));
     }
 
@@ -98,7 +98,9 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        // dd($owner);
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
