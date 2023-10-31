6b07672e354ad0ee0a74a61e0578e9d334e50695
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-31 16:23:41 +0900

    ページネーション作成

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -63,6 +63,7 @@
                                         @endforeach
                                     </tbody>
                                 </table>
+                                {{ $owners->links() }}
                             </div>
                         </div>
                     </section>
