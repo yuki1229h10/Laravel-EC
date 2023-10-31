07c9b066f3d5a28d81273daa68f86a65e4ccd9c1
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-31 16:44:02 +0900

    スマホ対応

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -8,10 +8,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="md:p-6 bg-white border-b border-gray-200">
 
                     <section class="text-gray-600 body-font">
-                        <div class="container px-5 mx-auto">
+                        <div class="container md:px-5 mx-auto">
                             <x-flash-message status="session('status')" />
                             <div class="flex justify-end mb-4">
                                 <button onclick="location.href='{{ route('admin.owners.create') }}'"
@@ -22,29 +22,29 @@
                                     <thead>
                                         <tr>
                                             <th
-                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                                                class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
                                                 名前</th>
                                             <th
-                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                                                 メールアドレス</th>
                                             <th
-                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                                                 作成日</th>
                                             <th
-                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+                                                class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                                             </th>
                                             <th
-                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+                                                class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                                             </th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($owners as $owner)
                                             <tr>
-                                                <td class="px-4 py-3">{{ $owner->name }}</td>
-                                                <td class="px-4 py-3">{{ $owner->email }}</td>
-                                                <td class="px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
-                                                <td class="px-4 py-3">
+                                                <td class="md:px-4 py-3">{{ $owner->name }}</td>
+                                                <td class="md:px-4 py-3">{{ $owner->email }}</td>
+                                                <td class="md:px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
+                                                <td class="md:px-4 py-3">
                                                     <button
                                                         onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id]) }}'"
                                                         class="text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded">編集</button>
