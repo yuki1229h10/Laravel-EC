c1c004b59b10e8275101e2aa577d3c77a724be26
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-31 17:58:23 +0900

    fix

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
-use\App\Models\Shop;
+use App\Models\Shop;
 
 class Owner extends Authenticatable
 {
